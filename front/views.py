4e1ffeec16a4a8522de60862c56594ff94f0132d
Dmitry Barskov <barskoidim@gmail.com>
2018-07-15 17:12:37 +0300

    added some views, urls

--- front/views.py
+++ front/views.py
@@ -1,32 +1,50 @@
 from django.contrib.auth import logout
 from django.shortcuts import render, redirect
 
+from front.models import (
+    TaskSet
+)
+
 
 def logout_view(request):
     logout(request)
-    return redirect('../test/')
-
-
-def login_page(request):
-    return render(request, 'front/login.html', {})
-
-
-def task_set(request):
-    return render(request, 'front/TaskSet.html', {})
-
-
-def index(request):
-    # todo create main page
-    return render(request, 'front/bitbucket.html', {})
-
-
-def test(request):
-    return render(request, 'front/test.html', {'user': request.user})
-
-
-def profile_view(request):
     return redirect('index')
 
 
-def students_list(request):
-    return render(request, 'front/listOfStudents.html', {})
+def task_sets(request):
+    task_sets_query = TaskSet.objects.filter(author=request.user).order_by('number')
+    return render(
+        request,
+        'front/TaskSets.html',
+        {
+            'user': request.user,
+            'tasksets': task_sets_query,
+        }
+    )
+
+
+def task_set(request, number: int):
+    taskset = TaskSet.objects.filter(author=request.user).get(number=number)
+    return render(
+        request,
+        'front/TaskSet.html',
+        {
+            'user': request.user,
+            'taskset': taskset,
+        }
+    )
+
+
+def index(request):
+    return redirect('tasksets')
+
+
+def test(request):
+    # todo
+    return render(
+        request,
+        'front/test.html',
+        {
+            'user': request.user,
+        }
+    )
